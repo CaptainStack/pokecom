689fac3dfe773bd397174a4c027fec37282fdc4e
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-24 03:12:41 -0700

    Add list action to posts controller.

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -9,4 +9,10 @@ module.exports = {
       .then(post => response.status(201).send(post))
       .catch(error => response.status(400).send(error));
   },
+  list(request, response) {
+    return Post
+      .all()
+      .then(posts => response.status(200).send(posts))
+      .catch(error => response.status(400).send(error));
+  },
 };
