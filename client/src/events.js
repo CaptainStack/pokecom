55d5568aba66e3f2637347b7873a072a18092dac
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-24 10:57:10 -0700

    Add Redux flows for new post creation.

--- client/src/events.js
+++ client/src/events.js
@@ -13,3 +13,24 @@ export const delete_post_button_clicked = post => event => {
       store.dispatch({ type: 'DELETE_POST', post: deleted_post });
     });
 }
+
+export const new_post_form_changed = () => event => {
+  store.dispatch({ type: 'UPDATE_POST_FORM_CONTENT', content: event.target.value });
+}
+
+export const create_post_button_clicked = post_content => event => {
+  fetch(`/api/posts/`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+      body: JSON.stringify({
+        content: post_content
+      })
+  }).then(response => response.json())
+    .then(created_post => {
+      store.dispatch({ type: 'CREATE_NEW_POST', post: created_post });
+    });
+  event.preventDefault();
+}
--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,4 +1,4 @@
-import { update_posts_list, delete_post, INITIAL_STATE } from './core';
+import { update_posts_list, delete_post, create_new_post, update_post_form_content, INITIAL_STATE } from './core';
 
 export default function reducer(state = INITIAL_STATE, action) {
   switch (action.type) {
@@ -6,6 +6,10 @@ export default function reducer(state = INITIAL_STATE, action) {
       return update_posts_list(state, action.posts);
     case 'DELETE_POST':
       return delete_post(state, action.post);
+    case 'CREATE_NEW_POST':
+      return create_new_post(state, action.post);
+    case 'UPDATE_POST_FORM_CONTENT':
+      return update_post_form_content(state, action.content);
     default:
       return state;
   }
