b40654a9f6dffcd4db5687a904eb01741f2599aa
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-24 02:26:17 -0700

    Add parsing, logging, and routes to app.js

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+require('./server/routes')(app);
 app.get('*', (request, response) => response.status(200).send({
   message: 'Success'
 }));
