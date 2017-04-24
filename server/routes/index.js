24e5128f06131c1325cccd1bde2ee551a35cce4c
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-24 02:27:01 -0700

    Add create post route.

---
+++ server/routes/index.js
@@ -0,0 +1,9 @@
+const postsController = require('../controllers').posts;
+
+module.exports = app => {
+  app.get('/api', (request, response) => response.status(200).send({
+    message: 'Welcome to the Pokecom API!',
+  }));
+
+  app.post('/api/posts', postsController.create);
+};
