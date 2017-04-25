d9e77c29c561a4afbe753e20b86feb2383f6c6e5
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-25 04:22:55 -0700

    Refactor Post component.

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import '../styles/Post.css';
 
-export const Post = ({ post, delete_button_action }) => <li className='Post'>{ post.content } <br /> Posted by: {post.pokemon}<button onClick={ delete_button_action }>Delete Post</button></li>;
+export const Post = ({ post, delete_button_action }) => 
+  <div className='Post'>
+    <h3>{ post.content }</h3>
+    <p>Posted by: {post.pokemon} [<span onClick={ delete_button_action }>delete</span>]</p>
+  </div>
 
 export default Post;
