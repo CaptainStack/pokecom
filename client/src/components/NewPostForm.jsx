06e6ed2156e375c950269ae6bd34bac95a63d31e
Andre Stackhouse <CaptainStack@outlook.com>
2017-04-25 04:23:34 -0700

    Refactor NewPostForm component.

--- client/src/components/NewPostForm.jsx
+++ client/src/components/NewPostForm.jsx
@@ -4,8 +4,9 @@ import { new_post_form_changed, create_post_button_clicked } from '../events';
 export const NewPostForm = ({ form_content }) => {
   return(
     <form className='NewPostForm'>
+      <h2>Create new post</h2>
       <textarea onChange={ new_post_form_changed() } placeholder='Compose Post' value={ form_content }></textarea>
-      <button onClick={ create_post_button_clicked(form_content) }>Submit</button>
+      <div><button onClick={ create_post_button_clicked(form_content) }>Submit</button></div>
     </form>
   );
 }
